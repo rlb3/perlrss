def9f3a0dfbbd07fafde7a2c2eddb4304e73eaea
Robert Boone <robo4288@gmail.com>
2009-06-29 22:59:58 -0500

    Build dictionary data structure to hold perl modules

--- perlrss.m
+++ perlrss.m
@@ -2,14 +2,32 @@
 #import "Rss.h"
 
 int main (int argc, const char * argv[]) {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    NSMutableArray *stash = [[NSMutableArray alloc] init];
 
     Rss *feed = [[Rss alloc] init];
     
     for (NSXMLElement *line in [[feed pull] itemNodes]) {
-        printf("%s\n", [[line stringValue] UTF8String]);
+        if ([[line stringValue] isNotEqualTo:@"search.cpan.org"]) {
+            NSMutableArray *tempArray = [NSMutableArray arrayWithArray:[[line stringValue] componentsSeparatedByString:@"-"]];
+            NSString *version = [tempArray lastObject];
+            [tempArray removeLastObject];
+
+            NSString *moduleName = [tempArray componentsJoinedByString:@"::"];
+            NSMutableDictionary *tempDict = [[NSMutableDictionary alloc] init];
+
+            [tempDict setObject:moduleName forKey:@"name"];
+            [tempDict setObject:version forKey:@"version"];
+
+            [stash addObject:tempDict];
+
+            [tempDict release];
+        }
     }
-    
+
+    NSLog(@"%@", stash);
+
+    [stash release];
     [pool drain];
     return 0;
 }
