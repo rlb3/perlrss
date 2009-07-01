3fe441dac60744bb03a10bf67af99d2b9e4762b0
Robert Boone <robo4288@gmail.com>
2009-07-01 13:44:26 -0500

    Add seen modules db and check against it before adding

--- perlrss.m
+++ perlrss.m
@@ -2,14 +2,24 @@
 #import "Rss.h"
 
 int main (int argc, const char * argv[]) {
-    NSAutoreleasePool *pool    = [[NSAutoreleasePool alloc] init];
-    NSMutableArray    *data    = [[[Rss alloc] init] fetchData];
-    NSArray           *modules = [NSArray arrayWithContentsOfFile:@"/Users/robert/projects/perlrss/modules.plist"];
+    NSAutoreleasePool *pool          = [[NSAutoreleasePool alloc] init];
+    NSMutableArray    *updateModules = [[[Rss alloc] init] fetchData];
+    NSArray           *modules       = [NSArray arrayWithContentsOfFile:@"/Users/robert/project/perlrss/modules.plist"];
+    NSMutableArray    *seen          = [NSMutableArray arrayWithContentsOfFile:@"/Users/robert/project/perlrss/seen.plist"];
 
+    for (NSDictionary *dict in updateModules) {
+        NSString *fullModule = [[dict objectForKey:@"name"] stringByAppendingString:[dict objectForKey:@"version"]];
+        if ([seen containsObject:fullModule] == NO) {
+            if ([modules containsObject:[dict objectForKey:@"name"]]) {
+                // Send email
+                [seen addObject:fullModule];
+            }
+        }
+    }
 
-    NSLog(@"%@", modules);
+    [seen writeToFile:@"/Users/robert/project/perlrss/seen.plist" atomically:YES];
 
-    [data release];
+    [updateModules release];
     [pool drain];
     return 0;
 }
